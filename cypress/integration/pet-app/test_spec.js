6d3838f70c610f674b65ac94bf16dd90f188fc4d
unknown <chirlychplin@gmail.com>
2021-12-20 18:33:47 -0400

    app testeada

--- cypress/integration/pet-app/test_spec.js
+++ cypress/integration/pet-app/test_spec.js
@@ -1,8 +1,17 @@
 /* global describe, ir, expect */
 
- 
-describe('mi primer test', function() {
-    it('para ver si funciona', function (){
-        expect(true).to.equal(true)
+describe('Petgram', function () {
+    it('Funcionamiento de rutas y Modales', function () {
+        cy.visit('/')
+        cy.get('#historiaBtn').contains('Siguiente').click()
+        cy.get('button').contains('Entrar').click()
+        cy.get('div').contains('Rocky').click()
+        cy.get('.modalPet__exit').click()
+        cy.get('nav').contains('Favoritos').click()
+        cy.get('nav').contains('Mensajes').click()
+        cy.get('.chat').click()
+        cy.get('.modaluser__exit').click()
+        cy.get('nav').contains('Perfil').click()
+        cy.get('button').contains('Salir').click()
     })
 })
